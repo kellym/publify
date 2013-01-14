afdcc668356ef703c9585f268f2e05c9ed285f47
Matijs van Zuijlen <matijs@matijs.net>
2013-01-14 19:33:08 +0100

    Disable useless migration

--- db/migrate/097_fix_nofollow_options_behavior.rb
+++ db/migrate/097_fix_nofollow_options_behavior.rb
@@ -1,18 +1,24 @@
 class FixNofollowOptionsBehavior < ActiveRecord::Migration
   def self.up
-    unless Blog.default.nil?
-      Blog.default.unindex_categories = false if Blog.default.index_categories
-      Blog.default.unindex_tags = false if Blog.default.index_tags
-      Blog.default.dofollowify = true unless Blog.default.nofollowify
-      Blog.default.save!
+    # FIXME: Migration disabled because it did nothing. Needs rewrite.
+    if false
+      unless Blog.default.nil?
+        Blog.default.unindex_categories = false if Blog.default.index_categories
+        Blog.default.unindex_tags = false if Blog.default.index_tags
+        Blog.default.dofollowify = true unless Blog.default.nofollowify
+        Blog.default.save!
+      end
     end
   end
 
   def self.down
-    blog = Blog.first
-    Blog.default.index_categories = true unless Blog.default.unindex_categories
-    Blog.default.index_tags = true unless Blog.default.unindex_tags
-    Blog.default.dofollowify = true unless Blog.default.nofollowify
-    Blog.default.save!
+    # FIXME: Migration disabled because it did nothing. Needs rewrite.
+    if false
+      blog = Blog.first
+      Blog.default.index_categories = true unless Blog.default.unindex_categories
+      Blog.default.index_tags = true unless Blog.default.unindex_tags
+      Blog.default.dofollowify = true unless Blog.default.nofollowify
+      Blog.default.save!
+    end
   end
 end
